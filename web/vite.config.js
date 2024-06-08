b00af1b1d0bcb2137ed33e6ed677e8d2829ff3e4
Robert Vilcu <robertvilcu89@gmail.com>
2024-06-08 23:16:56 +0100

    update: vite.config.js to disable chunking and include dynamic imports in the main bundle

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './',
-	build: {
-		outDir: "dist",
-		chunkSizeWarningLimit: 1600,
-	},
+    build: {
+        outDir: "dist",
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined, // disable chunking / code splitting
+                inlineDynamicImports: true, // Ensures dynamic imports are included in the main bundle
+            },
+        },
+    },
 	define: {
 		global: {}
 	},
