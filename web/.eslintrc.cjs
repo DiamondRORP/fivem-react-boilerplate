b349102fdfc98bfa636946b9e81c2e9dc17764ab
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-08-14 19:14:39 +0100

    Create .eslintrc.cjs

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'risxss'],
+  rules: {
+    'risxss/catch-potential-xss-react': 'error',  //  <<< add this in rules
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    indent: 'off', // prettier does it
+    'no-tabs': ['off', { allowIndentationTabs: true }], // i want tabs
+  },
+}
