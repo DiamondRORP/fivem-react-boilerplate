d55d7aeb1a2fc3801ca7dd7fda86c74183935928
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-08-28 16:53:21 +0100

    update: tailwind.config.js - added textShadow

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				0: '0 0 0 rgba(0, 0, 0, 0)',
+				'sm': '1px 4px 2px var(--tw-shadow-color)',
+				'lg': '0 4px 18px var(--tw-shadow-color)',
+			},
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+    			matchUtilities(
+    				{
+    					'text-shadow': (value) => ({
+    						textShadow: value,
+    					}),
+    				},
+    				{ values: theme('textShadow') }
+    			)
+    }),
+],
 }
